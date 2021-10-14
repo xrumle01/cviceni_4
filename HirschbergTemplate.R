199cd08a05665a0702e198386435eea50f35bec2
Tereza Rumlerova <xrumle01@vutbr.cz>
2021-10-14 14:29:24 +0200

    Add Hirschberg algorithm

--- HirschbergTemplate.R
+++ HirschbergTemplate.R
@@ -1,67 +1,56 @@
-Hirschberg_sablona <- function(X,Y,Align,match,mismatch,gap){
+Hirschberg_sablona <- function(X,Y,Align,match,mismatch,gap){ 
   Z <- Align[[1]] #inicializace prvniho radku zarovnani, typ DNAStringSet
-  W <- Align[[2]] #inicializace druheho radku zarovnani
+  W <- Align[[2]] #inicializace druheho radku zarovnani 
   
-  if #delka X je nula
-  {
-    for #podle poctu znaku v Y
-    {
-      Z <- #prida se mezera
-        W <- #prida se znak z Y
+  if(length(X) == 0){  #delka X je nula
+    for(i in 1:length(Y)){  #podle poctu znaku v Y
+      Z[[1]] <- c(Z[[1]], '-')  #prida se mezera
+      W[[1]] <- c(W[[1]], Y[i])  #prida se znak z Y
     }
     Align <- c(DNAStringSet(Z),DNAStringSet(W))
     print(Align)
   }
-  else if #delka Y je nula
-  {
-    for #podle poctu znaku v X
-    {
-      Z <- #prida se znak z X
-        W <- #prida se mezera
+  else if(length(Y) == 0){  #delka Y je nula
+    for(j in 1:length(X)){  #podle poctu znaku v X
+      Z[[1]] <- c(Z[[1]], X[j])  #prida se znak z X
+      W[[1]] <- c(W[[1]], '-')  #prida se mezera
     }
     Align <- c(DNAStringSet(Z),DNAStringSet(W))
     print(Align)
   }
-  else if #delka X a Y je jedna
-  {
-    Z <- #prida se znak z X
-      W <- #prida se znak z Y
-      Align <- c(DNAStringSet(Z),DNAStringSet(W))
+  else if(length(X) == 1 & length(Y) == 1){  #delka X a Y je jedna
+    Z <- X  #prida se znak z X
+    W <- Y  #prida se znak z Y
+    Align <- c(DNAStringSet(Z),DNAStringSet(W))
     print(Align)
   }
-  else
-  {
-    xlen <- #delka X
-      xmid <- #pulka delky X
-      ylen <- #delka Y
+  else{
+      xlen <- length(X)  #delka X
+      xmid <- xlen/2  #pulka delky X
+      ylen <- length(Y)  #delka Y
       
-      ScoreL <- # NW skore pro prvni polovinu X a cele Y
-      ScoreR <- # NW skore pro druhou polovinu X a cele Y (obe sek jsou obracene)
-      ymid <- #index deleni Y
+      ScoreL <- Needleman_wunsch(X[1:xmid], Y, match, mismatch, gap)  # NW skore pro prvni polovinu X a cele Y
+      ScoreR <- Needleman_wunsch(reverse(X[(xmid+1):xlen]), reverse(Y), match, mismatch, gap)  # NW skore pro druhou polovinu X a cele Y (obe sek jsou obracene)
+      ymid <- which.max(ScoreL + rev(ScoreR)) - 1 #index deleni Y
       
       #pro prvni cast
-      if #index deleni Y je nula
-    {
-      Align <- #volani Hirschberg pro prvni polovinu X a prazdny vektor Y
-    }
-    else
-    {
-      Align <- #volani Hirschberg pro prvni polovinu X a prvni cast Y
-    }
+      if(ymid == 0){  #index deleni Y je nula
+      Align <- Hirschberg_sablona(X[1:xmid], c(), Align, match, mismatch, gap)  #volani Hirschberg pro prvni polovinu X a prazdny vektor Y
+      }
+      else{
+      Align <- Hirschberg_sablona(X[1:xmid], Y[1:ymid], Align, match, mismatch, gap)  #volani Hirschberg pro prvni polovinu X a prvni cast Y
+      }
     
-    #pro druhou cast
-    if ((xmid+1)>xlen) #X jiz nelze pulit
-    {
-      Align <- #volani Hirschberg pro prazdny vektor X a druhou cast Y
-    }
-    else if ((ymid+1)>ylen) #Y jiz nejde delit
-    {
-      Align <- #volani Hirschberg pro druhou polovinu X a prazdny vektor Y
-    }
-    else 
-    {
-      Align <- #volani Hirschberg pro druhou polovinu X a druhou cast Y
-    }
+      #pro druhou cast
+      if ((xmid+1)>xlen){  #X jiz nelze pulit
+      Align <- Hirschberg_sablona(c(), Y[ymid+1:ylen], Align, match, mismatch, gap)  #volani Hirschberg pro prazdny vektor X a druhou cast Y
+      }
+      else if ((ymid+1)>ylen){  #Y jiz nejde delit
+      Align <- Hirschberg_sablona(X[xmid+1:xlen], c(), Align, match, mismatch, gap)  #volani Hirschberg pro druhou polovinu X a prazdny vektor Y
+      }
+      else{
+      Align <- Hirschberg_sablona(X[xmid+1:xlen], Y[ymid+1:ylen], Align, match, mismatch, gap)  #volani Hirschberg pro druhou polovinu X a druhou cast Y
+      }
   }
   return(Align)
 }
